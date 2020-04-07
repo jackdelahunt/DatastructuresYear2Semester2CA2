a58d128b434c06e0664367dac10d91f8c5de2f71
StevenGuiry <46968144+StevenGuiry@users.noreply.github.com>
2020-04-07 12:33:47 +0100

    Marker-Button-Added
    Image selected and loaded on start-up
    Red marker displayed when user clicks on map.
    Coordinates show on right panel

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Rome Route Finder");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
