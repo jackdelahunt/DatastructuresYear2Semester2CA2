6e438b4e4a2a9df791b1614605328b2ac1517b3c
Jack Delahunt <20085409@mail.wit.ie>
2020-06-17 00:44:03 +0100

    removed animation changes, it was not working out correctly

--- src/SettingsController.java
+++ src/SettingsController.java
@@ -8,21 +8,19 @@ public class SettingsController {
     private TextField colourField;
 
     @FXML
-    private CheckBox rainbowRoad, searchType, animation;
+    private CheckBox rainbowRoad, searchType;
 
     @FXML
     public void initialize() {
         colourField.setText(Settings.pathColour);
         rainbowRoad.setSelected(Settings.isFabulous);
         searchType.setSelected(Settings.searchType);
-        animation.setSelected(Settings.animation);
     }
 
     public void applyChanges() {
         Settings.pathColour = colourField.getText();
         Settings.isFabulous = rainbowRoad.isSelected();
         Settings.searchType = searchType.isSelected();
-        Settings.animation = animation.isSelected();
 
         close();
     }
