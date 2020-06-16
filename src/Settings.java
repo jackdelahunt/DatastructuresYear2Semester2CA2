8341578545f0d044a32495a12ce8f423d9632a55
Jack Delahunt <20085409@mail.wit.ie>
2020-06-17 00:11:30 +0100

    added animaion setting *not working yet*

--- src/Settings.java
+++ src/Settings.java
@@ -2,4 +2,5 @@ public class Settings {
     public static String pathColour = "#ff0000";
     public static boolean isFabulous = false;
     public static boolean searchType = true; // using bfs is true
+    public static boolean animation = false;
 }
--- src/SettingsController.java
+++ src/SettingsController.java
@@ -8,19 +8,21 @@ public class SettingsController {
     private TextField colourField;
 
     @FXML
-    private CheckBox rainbowRoad, searchType;
+    private CheckBox rainbowRoad, searchType, animation;
 
     @FXML
     public void initialize() {
         colourField.setText(Settings.pathColour);
         rainbowRoad.setSelected(Settings.isFabulous);
         searchType.setSelected(Settings.searchType);
+        animation.setSelected(Settings.animation);
     }
 
     public void applyChanges() {
         Settings.pathColour = colourField.getText();
         Settings.isFabulous = rainbowRoad.isSelected();
         Settings.searchType = searchType.isSelected();
+        Settings.animation = animation.isSelected();
 
         close();
     }
